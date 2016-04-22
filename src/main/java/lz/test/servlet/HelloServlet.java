1768eea22737a970680666099481f82bc3467b98
Zheng Liu <cszliu@gmail.com>
2016-04-22 16:33:04 +0800

    clean code

--- src/main/java/lz/test/servlet/HelloServlet.java
+++ src/main/java/lz/test/servlet/HelloServlet.java
@@ -1,9 +1,5 @@
 package lz.test.servlet;
 
-import lz.test.service.HelloWorldService;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,10 +16,6 @@ public class HelloServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter outer = resp.getWriter();
         outer.println("I love siyu.");
-
-        WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
-        HelloWorldService helloService = (HelloWorldService) context.getBean("HelloWorldConsumer");
-        outer.println(helloService.sayHello("siyu"));
         outer.close();
     }
 }
