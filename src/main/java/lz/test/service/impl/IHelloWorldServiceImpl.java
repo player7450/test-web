d8341219a35cdb471bb416ea7d042c906480c277
Zheng Liu <cszliu@gmail.com>
2016-06-19 00:21:24 +0800

    hehe

--- src/main/java/lz/test/service/impl/IHelloWorldServiceImpl.java
+++ src/main/java/lz/test/service/impl/IHelloWorldServiceImpl.java
@@ -1,11 +1,11 @@
 package lz.test.service.impl;
 
-import lz.test.service.HelloWorldService;
+import lz.test.service.IHelloWorldService;
 
 /**
  * Created by liuzheng on 16/4/19.
  */
-public class HelloWorldServiceImpl implements HelloWorldService {
+public class IHelloWorldServiceImpl implements IHelloWorldService {
     public String sayHello(String name) {
         return "hi, " + name + "\n";
     }
