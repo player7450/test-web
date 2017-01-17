70ca46aa8b3c7e76f8c8c5f06fe88e25d9069f59
player7450 <cszliu@gmail.com>
2017-01-17 15:57:42 +0800

    update service name

--- src/main/java/lz/test/service/impl/HelloServiceImpl.java
+++ src/main/java/lz/test/service/impl/HelloServiceImpl.java
@@ -1,11 +1,11 @@
 package lz.test.service.impl;
 
-import lz.test.service.IHelloWorldService;
+import lz.test.service.HelloService;
 
 /**
  * Created by liuzheng on 16/4/19.
  */
-public class HelloWorldServiceImpl implements IHelloWorldService {
+public class HelloServiceImpl implements HelloService {
     public String sayHello(String name) {
         return "hi, " + name + "\n";
     }
