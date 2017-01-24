e111368c405ee8e0f59efeaedc6e58f77875054f
player7450 <cszliu@gmail.com>
2017-01-24 15:23:35 +0800

    add service

--- src/main/java/lz/test/service/impl/HelloServiceImpl.java
+++ src/main/java/lz/test/service/impl/HelloServiceImpl.java
@@ -1,10 +1,12 @@
 package lz.test.service.impl;
 
 import lz.test.service.HelloService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by liuzheng on 16/4/19.
  */
+@Service(value = "helloService")
 public class HelloServiceImpl implements HelloService {
     public String sayHello(String name) {
         return "hi, " + name + "\n";
